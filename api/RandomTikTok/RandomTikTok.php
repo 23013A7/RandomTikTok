a186fbd42a6260e47cf834a836742873b083ed1d
23013A7 <23013a72010@gmail.com>
2026-01-13 20:35:30 +0300

    Обновлён RandomTikTok.php
    Добавлена обработка нового JSON формата хранения тик токов

--- api/RandomTikTok/RandomTikTok.php
+++ api/RandomTikTok/RandomTikTok.php
@@ -4,8 +4,9 @@
     header("Content-Type: application/json; charset=UTF-8");//Установка того что апи вернёт json
     //Определение какой http метод используется
 
-    $filename = '../../statick/RandomTikTok/RandomTikTok.txt'; //запись имени файла в переменую
-    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //получения массива строк из файла
-    $randomkey = array_rand($lines);
-    echo json_encode(["TikTok" => $lines[$randomkey]], JSON_UNESCAPED_UNICODE);//возрат json
+    $filename = '../../statick/RandomTikTok/RandomTikTok.json'; //запись имени файла в переменую
+    $jsoncontent = file_get_contents($filename); //получения содержимого файла
+    $data = json_decode($jsoncontent, true); //декодирования джейсона
+    $randomkey = array_rand($data); //получения случаеного ключа
+    echo json_encode(["TikTok" => $data[$randomkey]], JSON_UNESCAPED_UNICODE);//возрат json
 ?>
